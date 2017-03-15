0e4ce998652c8acb8d2a82d6b4fe2139e2897cde
Sean Carroll <sfcarroll@gmail.com>
2017-03-15 08:21:04 +0100

    removes deprecation warnings in controller specs

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,16 +1,19 @@
 require 'spec_helper'
+
 module TranslationCenter
 	describe CategoriesController, :type => :controller do
-	  let(:user) {FactoryGirl.create(:user)}
-	  before(:each) do
+    routes { TranslationCenter::Engine.routes }
+
+	  let(:user) { FactoryGirl.create(:user) }
+
+    before(:each) do
 	    sign_in user
-   	  end
+   	end
 
 	  describe "GET index" do
 	    it "assigns @categories & render index template" do
 	      categories = Category.all
-
-	      get :index, use_route: "translation_center"
+	      get :index
 	      expect(assigns(:categories)).to eq(categories)
 	      expect(response).to render_template(:index)
 	    end
@@ -18,9 +21,8 @@ module TranslationCenter
 
 	 describe "GET show" do
 	    it "assigns @category & render show template" do
-	  	category = FactoryGirl.create(:category)
-
-	      get :show, id: category.id, use_route: "translation_center"
+	  	  category = FactoryGirl.create(:category)
+	      get :show, id: category.id
 	      expect(assigns(:category)).to eq(category)
 	      expect(response).to render_template(:show)
 	    end
@@ -29,17 +31,17 @@ module TranslationCenter
 	  describe "GET more_keys" do
 	  	it "assigns @category & render keys", :js => true do
 	  	  category = FactoryGirl.create(:category)
-	  	  get :more_keys, category_id: category.id, format: :js, use_route: "translation_center"
-	      	  expect(response).to render_template(:keys)
+	  	  get :more_keys, category_id: category.id, format: :js
+	      expect(response).to render_template(:keys)
 	  	end
 	  end
 
-	    describe "GET destroy" do
+	  describe "GET destroy" do
 	  	it "assigns @category & render index" do
 	  	  category = FactoryGirl.create(:category)
-	        get :destroy, id: category.id, use_route: "translation_center"
-	      	  expect(Category.where(id: category.id)).to be_empty
-	      	  expect(response).to redirect_to(:categories)
+	      get :destroy, id: category.id
+	      expect(Category.where(id: category.id)).to be_empty
+	      expect(response).to redirect_to(:categories)
 	    end
 	  end
 
